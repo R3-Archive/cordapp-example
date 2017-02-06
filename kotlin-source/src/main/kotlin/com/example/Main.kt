c6c4a2342e56621e7b1b8c68145f90463268dc52
Joel Dudley <joel.dudley@r3cev.com>
2017-02-06 10:55:10 +0000

    Updates main to handle webserver separation.

--- kotlin-source/src/main/kotlin/com/example/Main.kt
+++ kotlin-source/src/main/kotlin/com/example/Main.kt
@@ -1,5 +1,7 @@
 package com.example
 
+import com.google.common.util.concurrent.Futures
+import net.corda.core.getOrThrow
 import net.corda.core.node.services.ServiceInfo
 import net.corda.node.driver.driver
 import net.corda.node.services.User
@@ -11,21 +13,28 @@ import net.corda.node.services.transactions.ValidatingNotaryService
  *
  * To debug your CorDapp:
  *
- * 1. Run the "Run Example CorDapp" run configuration.
+ * 1. Firstly, run the "Run Example CorDapp" run configuration.
  * 2. Wait for all the nodes to start.
- * 3. Note the debug ports for each node, which should be output to the console. The "Debug CorDapp" configuration runs
- *    with port 5007, which should be "NodeA". In any case, double-check the console output to be sure.
+ * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
+ *    5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
+ *    the console output to be sure.
  * 4. Set your breakpoints in your CorDapp code.
  * 5. Run the "Debug CorDapp" remote debug run configuration.
  */
 fun main(args: Array<String>) {
     // No permissions required as we are not invoking flows.
     val user = User("user1", "test", permissions = setOf())
-    driver(dsl = {
+    driver(isDebug = true) {
         startNode("Controller", setOf(ServiceInfo(ValidatingNotaryService.type)))
-        startNode("NodeA", rpcUsers = listOf(user))
-        startNode("NodeB", rpcUsers = listOf(user))
-        startNode("NodeC", rpcUsers = listOf(user))
+        val (nodeA, nodeB, nodeC) = Futures.allAsList(
+                startNode("NodeA", rpcUsers = listOf(user)),
+                startNode("NodeB", rpcUsers = listOf(user)),
+                startNode("NodeC", rpcUsers = listOf(user))).getOrThrow()
+
+        startWebserver(nodeA)
+        startWebserver(nodeB)
+        startWebserver(nodeC)
+
         waitForAllNodesToFinish()
-    }, isDebug = true)
+    }
 }
