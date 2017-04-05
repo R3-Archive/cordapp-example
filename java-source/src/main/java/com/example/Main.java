71e7a27a488a329ec46588dd75d87a9132535b0e
Patrick Kuo <patrick.kuo@r3.com>
2017-04-05 17:22:11 +0100

    [M10 bug fixes] - driver dsl bug fix

--- java-source/src/main/java/com/example/Main.java
+++ java-source/src/main/java/com/example/Main.java
@@ -4,8 +4,9 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import net.corda.core.node.services.ServiceInfo;
 import net.corda.node.driver.NodeHandle;
-import net.corda.nodeapi.User;
+import net.corda.node.services.config.VerifierType;
 import net.corda.node.services.transactions.ValidatingNotaryService;
+import net.corda.nodeapi.User;
 
 import static java.util.Collections.*;
 import static net.corda.node.driver.Driver.driver;
@@ -13,14 +14,14 @@ import static net.corda.node.driver.Driver.driver;
 /**
  * This file is exclusively for being able to run your nodes through an IDE (as opposed to running deployNodes)
  * Do not use in a production environment.
- *
+ * <p>
  * To debug your CorDapp:
- *
+ * <p>
  * 1. Firstly, run the "Run Example CorDapp" run configuration.
  * 2. Wait for all the nodes to start.
  * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
- *    5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
- *    the console output to be sure.
+ * 5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
+ * the console output to be sure.
  * 4. Set your breakpoints in your CorDapp code.
  * 5. Run the "Debug CorDapp" remote debug run configuration.
  */
@@ -34,12 +35,13 @@ public class Main {
                     dsl.startNode("Controller",
                             ImmutableSet.of(new ServiceInfo(ValidatingNotaryService.Companion.getType(), null)),
                             emptyList(),
+                            VerifierType.InMemory,
                             emptyMap());
 
                     try {
-                        NodeHandle nodeA = dsl.startNode("NodeA", emptySet(), ImmutableList.of(user), emptyMap()).get();
-                        NodeHandle nodeB = dsl.startNode("NodeB", emptySet(), ImmutableList.of(user), emptyMap()).get();
-                        NodeHandle nodeC = dsl.startNode("NodeC", emptySet(), ImmutableList.of(user), emptyMap()).get();
+                        NodeHandle nodeA = dsl.startNode("NodeA", emptySet(), ImmutableList.of(user), VerifierType.InMemory, emptyMap()).get();
+                        NodeHandle nodeB = dsl.startNode("NodeB", emptySet(), ImmutableList.of(user), VerifierType.InMemory, emptyMap()).get();
+                        NodeHandle nodeC = dsl.startNode("NodeC", emptySet(), ImmutableList.of(user), VerifierType.InMemory, emptyMap()).get();
 
                         dsl.startWebserver(nodeA);
                         dsl.startWebserver(nodeB);
