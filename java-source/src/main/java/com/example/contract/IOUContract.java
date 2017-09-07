e4f8cfeb4bd0ed93437d7105c326102bca114be3
joeldudleyr3 <joel.dudley@r3.com>
2017-09-07 10:57:57 +0100

    Addresses review. Removes AuthenticatedObject.

--- java-source/src/main/java/com/example/contract/IOUContract.java
+++ java-source/src/main/java/com/example/contract/IOUContract.java
@@ -1,13 +1,14 @@
 package com.example.contract;
 
 import com.example.state.IOUState;
-import net.corda.core.contracts.AuthenticatedObject;
 import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
 import net.corda.core.contracts.Contract;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.transactions.LedgerTransaction;
 
 import java.util.stream.Collectors;
+
 import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
@@ -30,7 +31,7 @@ public class IOUContract implements Contract {
      */
     @Override
     public void verify(LedgerTransaction tx) {
-        final AuthenticatedObject<Commands.Create> command = requireSingleCommand(tx.getCommands(), Commands.Create.class);
+        final CommandWithParties<Commands.Create> command = requireSingleCommand(tx.getCommands(), Commands.Create.class);
         requireThat(require -> {
             // Generic constraints around the IOU transaction.
             require.using("No inputs should be consumed when issuing an IOU.",
