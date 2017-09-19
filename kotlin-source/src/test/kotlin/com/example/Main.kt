b3b03a6072a1833ec7867c3bdf56ed648595b70f
Andrzej Cichocki <andrzej.cichocki@r3.com>
2017-09-19 08:18:29 +0100

    * Make run configs work if there are spaces in path to cordapp-tutorial * Satisfy X500 validation

--- kotlin-source/src/test/kotlin/com/example/Main.kt
+++ kotlin-source/src/test/kotlin/com/example/Main.kt
@@ -25,11 +25,11 @@ fun main(args: Array<String>) {
     // No permissions required as we are not invoking flows.
     val user = User("user1", "test", permissions = setOf())
     driver(isDebug = true) {
-        startNode(providedName = CordaX500Name("Controller", "corda", "R3", "London", null, "UK"), advertisedServices = setOf(ServiceInfo(ValidatingNotaryService.type)))
+        startNode(providedName = CordaX500Name("Controller", "corda", "R3 Ltd", "London", null, "GB"), advertisedServices = setOf(ServiceInfo(ValidatingNotaryService.type)))
         val (nodeA, nodeB, nodeC) = listOf(
-                startNode(providedName = CordaX500Name("NodeA", "NodeA", "London", "UK"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("NodeB", "NodeB", "New York", "US"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("NodeC", "NodeC", "Paris", "FR"), rpcUsers = listOf(user))).map { it.getOrThrow() }
+                startNode(providedName = CordaX500Name("PartyA", "PartyA", "London", "GB"), rpcUsers = listOf(user)),
+                startNode(providedName = CordaX500Name("PartyB", "PartyB", "New York", "US"), rpcUsers = listOf(user)),
+                startNode(providedName = CordaX500Name("PartyC", "PartyC", "Paris", "FR"), rpcUsers = listOf(user))).map { it.getOrThrow() }
 
         startWebserver(nodeA)
         startWebserver(nodeB)
