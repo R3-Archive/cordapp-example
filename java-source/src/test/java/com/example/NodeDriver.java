0ab27b47376ec61fde874d9daa5bc901a21083fd
joeldudley <joeldudley@outlook.com>
2018-02-15 21:21:13 +0000

    Updates build.gradle files and driver tests to V3.

--- java-source/src/test/java/com/example/NodeDriver.java
+++ java-source/src/test/java/com/example/NodeDriver.java
@@ -1,25 +1,25 @@
 package com.example;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
+import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.node.services.transactions.ValidatingNotaryService;
-import net.corda.nodeapi.User;
-import net.corda.nodeapi.internal.ServiceInfo;
 import net.corda.testing.driver.DriverParameters;
 import net.corda.testing.driver.NodeHandle;
 import net.corda.testing.driver.NodeParameters;
+import net.corda.testing.node.User;
 
 import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
 import static net.corda.testing.driver.Driver.driver;
 
 /**
- * This file is exclusively for being able to run your nodes through an IDE (as opposed to running deployNodes)
+ * This file is exclusively for being able to run your nodes through an IDE (as opposed to using deployNodes)
  * Do not use in a production environment.
  * <p>
  * To debug your CorDapp:
  * <p>
- * 1. Firstly, run the "Run Example CorDapp" run configuration.
+ * 1. Firstly, run the "Run Template CorDapp" run configuration.
  * 2. Wait for all the nodes to start.
  * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
  * 5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
@@ -31,31 +31,26 @@ public class NodeDriver {
     public static void main(String[] args) {
         // No permissions required as we are not invoking flows.
         final User user = new User("user1", "test", emptySet());
-        driver(new DriverParameters().setIsDebug(true), dsl -> {
-                    dsl.startNode(new NodeParameters()
-                            .setProvidedName(new CordaX500Name("Controller", "London", "GB"))
-                            .setAdvertisedServices(ImmutableSet.of(new ServiceInfo(ValidatingNotaryService.Companion.getType(), null))));
+        driver(new DriverParameters().setIsDebug(true).setWaitForAllNodesToFinish(true), dsl -> {
+                    CordaFuture<NodeHandle> partyAFuture = dsl.startNode(new NodeParameters()
+                            .setProvidedName(new CordaX500Name("PartyA", "London", "GB"))
+                            .setRpcUsers(ImmutableList.of(user)));
+                    CordaFuture<NodeHandle> partyBFuture = dsl.startNode(new NodeParameters()
+                            .setProvidedName(new CordaX500Name("PartyB", "New York", "US"))
+                            .setRpcUsers(ImmutableList.of(user)));
+                    CordaFuture<NodeHandle> partyCFuture = dsl.startNode(new NodeParameters()
+                            .setProvidedName(new CordaX500Name("PartyC", "Paris", "FR"))
+                            .setRpcUsers(ImmutableList.of(user)));
 
                     try {
-                        NodeHandle nodeA = dsl.startNode(new NodeParameters()
-                                .setProvidedName(new CordaX500Name("PartyA", "London", "GB"))
-                                .setRpcUsers(ImmutableList.of(user))).get();
-                        NodeHandle nodeB = dsl.startNode(new NodeParameters()
-                                .setProvidedName(new CordaX500Name("PartyB", "New York", "US"))
-                                .setRpcUsers(ImmutableList.of(user))).get();
-                        NodeHandle nodeC = dsl.startNode(new NodeParameters()
-                                .setProvidedName(new CordaX500Name("PartyC", "Paris", "FR"))
-                                .setRpcUsers(ImmutableList.of(user))).get();
-
-                        dsl.startWebserver(nodeA);
-                        dsl.startWebserver(nodeB);
-                        dsl.startWebserver(nodeC);
-
-                        dsl.waitForAllNodesToFinish();
+                        dsl.startWebserver(partyAFuture.get());
+                        dsl.startWebserver(partyBFuture.get());
+                        dsl.startWebserver(partyCFuture.get());
                     } catch (Throwable e) {
                         System.err.println("Encountered exception in node startup: " + e.getMessage());
                         e.printStackTrace();
                     }
+
                     return null;
                 }
         );
--- kotlin-source/src/test/kotlin/com/example/NodeDriver.kt
+++ kotlin-source/src/test/kotlin/com/example/NodeDriver.kt
@@ -2,39 +2,33 @@ package com.example
 
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
-import net.corda.node.services.transactions.ValidatingNotaryService
-import net.corda.nodeapi.User
-import net.corda.nodeapi.internal.ServiceInfo
+import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.driver
+import net.corda.testing.node.User
 
 /**
- * This file is exclusively for being able to run your nodes through an IDE (as opposed to running deployNodes)
+ * This file is exclusively for being able to run your nodes through an IDE (as opposed to using deployNodes)
  * Do not use in a production environment.
  *
  * To debug your CorDapp:
  *
- * 1. Firstly, run the "Run Example CorDapp" run configuration.
+ * 1. Run the "Run Template CorDapp" run configuration.
  * 2. Wait for all the nodes to start.
- * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
- *    5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
- *    the console output to be sure.
+ * 3. Note the debug ports for each node, which should be output to the console. The "Debug CorDapp" configuration runs
+ *    with port 5007, which should be "PartyA". In any case, double-check the console output to be sure.
  * 4. Set your breakpoints in your CorDapp code.
  * 5. Run the "Debug CorDapp" remote debug run configuration.
  */
 fun main(args: Array<String>) {
     // No permissions required as we are not invoking flows.
     val user = User("user1", "test", permissions = setOf())
-    driver(isDebug = true) {
-        startNode(providedName = CordaX500Name("Controller", "London", "GB"), advertisedServices = setOf(ServiceInfo(ValidatingNotaryService.type)))
-        val (nodeA, nodeB, nodeC) = listOf(
+    driver(DriverParameters(isDebug = true, waitForAllNodesToFinish = true)) {
+        val (partyA, partyB) = listOf(
                 startNode(providedName = CordaX500Name("PartyA", "London", "GB"), rpcUsers = listOf(user)),
                 startNode(providedName = CordaX500Name("PartyB", "New York", "US"), rpcUsers = listOf(user)),
                 startNode(providedName = CordaX500Name("PartyC", "Paris", "FR"), rpcUsers = listOf(user))).map { it.getOrThrow() }
 
-        startWebserver(nodeA)
-        startWebserver(nodeB)
-        startWebserver(nodeC)
-
-        waitForAllNodesToFinish()
+        startWebserver(partyA)
+        startWebserver(partyB)
     }
 }
