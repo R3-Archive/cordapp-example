12f328bb990cd03d0dcbe214d89a83d73c3a3200
Joel Dudley <joel.dudley@r3cev.com>
2018-06-04 23:07:35 +0100

    Update comment to reflect run config name change.

--- java-source/src/test/java/com/example/NodeDriver.java
+++ java-source/src/test/java/com/example/NodeDriver.java
@@ -17,7 +17,7 @@ import static net.corda.testing.driver.Driver.driver;
  *
  * To debug your CorDapp:
  * 
- * 1. Firstly, run the "Run Template CorDapp" run configuration.
+ * 1. Firstly, run the "Run Example CorDapp - Java" run configuration.
  * 2. Wait for all the nodes to start.
  * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
  * 5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
