fa94295b10655964a33216cdab7e52c5a6739fc6
Michele Sollecito <michele.sollecito@r3.com>
2018-07-10 17:45:02 +0100

    Managed to reproduce the issue.

--- kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
+++ kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
@@ -1,18 +1,21 @@
 package com.example
 
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.internal.concurrent.map
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.TestIdentity
+import net.corda.testing.core.singleIdentity
 import net.corda.testing.driver.DriverParameters
 import net.corda.testing.driver.driver
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
+import org.eclipse.jetty.http.HttpStatus
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class DriverBasedTests {
     val bankA = TestIdentity(CordaX500Name("BankA", "", "GB"))
     val bankB = TestIdentity(CordaX500Name("BankB", "", "US"))
+    val bankC = TestIdentity(CordaX500Name("BankC", "", "GB"))
 
     @Test
     fun `node test`() {
@@ -55,6 +58,31 @@ class DriverBasedTests {
 
                 assertEquals("[ ]", response.body().string())
             }
+        }
+    }
+
+    @Test
+    fun `create IOU from http`() {
+        driver(DriverParameters(isDebug = true, startNodesInProcess = false)) {
+            val (nodeA, webserverA) = startNode(providedName = bankA.name).map { it to startWebserver(it).getOrThrow() }.getOrThrow()
+            val (nodeB, webserverB) = startNode(providedName = bankB.name).map { it to startWebserver(it).getOrThrow() }.getOrThrow()
+            val (nodeC, webserverC) = startNode(providedName = bankC.name).map { it to startWebserver(it).getOrThrow() }.getOrThrow()
+
+            val url = HttpUrl.Builder()
+                    .scheme("http")
+                    .host(webserverA.listenAddress.host)
+                    .port(webserverA.listenAddress.port)
+                    .addPathSegments("api/example/create-iou")
+                    .addQueryParameter("iouValue", "40")
+                    .addQueryParameter("partyName", nodeB.nodeInfo.singleIdentity().name.toString())
+                    .build()
+
+            val request = Request.Builder().url(url).put(RequestBody.create(MediaType.parse("application/json"), "")).build()
+
+            val client = OkHttpClient()
+            val response = client.newCall(request).execute()
+
+            assertEquals(HttpStatus.CREATED_201, response.code())
         }
     }
 }
