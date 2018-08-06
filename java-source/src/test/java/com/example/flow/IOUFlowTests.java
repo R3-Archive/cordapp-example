36da3955c33a7dfba3a1347b8a08189ea3df4697
joeldudleyr3 <joel.dudley@r3.com>
2018-08-06 17:04:30 +0100

    Build errors.

--- java-source/src/test/java/com/example/flow/IOUFlowTests.java
+++ java-source/src/test/java/com/example/flow/IOUFlowTests.java
@@ -1,11 +1,7 @@
 package com.example.flow;
 
-import com.example.state.IOUState;
 import com.google.common.collect.ImmutableList;
 import net.corda.core.concurrent.CordaFuture;
-import net.corda.core.contracts.ContractState;
-import net.corda.core.contracts.StateAndRef;
-import net.corda.core.contracts.TransactionState;
 import net.corda.core.contracts.TransactionVerificationException;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.testing.node.MockNetwork;
@@ -16,10 +12,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertEquals;
 
 public class IOUFlowTests {
     private MockNetwork network;
@@ -43,18 +36,12 @@ public class IOUFlowTests {
         network.stopNodes();
     }
 
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
-
     @Test
     public void flowRejectsInvalidIOUs() throws Exception {
         // The IOUContract specifies that IOUs cannot have negative values.
-        ExampleFlow.Initiator flow = new ExampleFlow.Initiator(1, b.getInfo().getLegalIdentities().get(0));
-        CordaFuture<SignedTransaction> future = a.startFlow(flow);
+        ExampleFlow.Initiator flow = new ExampleFlow.Initiator(b.getInfo().getLegalIdentities().get(0));
+        CordaFuture<Void> future = a.startFlow(flow);
         network.runNetwork();
-
-        // The IOUContract specifies that IOUs cannot have negative values.
-        exception.expectCause(instanceOf(TransactionVerificationException.class));
         future.get();
     }
 }
