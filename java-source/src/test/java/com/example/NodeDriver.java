bc4a7789994e2ef4bb1aa7f2349b33fbbbd935db
joeldudleyr3 <joel.dudley@r3.com>
2018-08-10 17:08:07 +0100

    Hardcodes driver web and RPC ports.

--- java-source/src/test/java/com/example/NodeDriver.java
+++ java-source/src/test/java/com/example/NodeDriver.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.identity.CordaX500Name;
@@ -9,40 +10,37 @@ import net.corda.testing.driver.NodeHandle;
 import net.corda.testing.driver.NodeParameters;
 import net.corda.testing.node.User;
 
+import java.util.List;
+
 import static net.corda.testing.driver.Driver.driver;
 
 /**
- * This file is exclusively for being able to run your nodes through an IDE (as opposed to using deployNodes)
+ * This file is exclusively for being able to run your nodes through an IDE.
  * Do not use in a production environment.
- *
- * To debug your CorDapp:
- * 
- * 1. Firstly, run the "Run Example CorDapp - Java" run configuration.
- * 2. Wait for all the nodes to start.
- * 3. Note the debug ports which should be output to the console for each node. They typically start at 5006, 5007,
- * 5008. The "Debug CorDapp" configuration runs with port 5007, which should be "NodeB". In any case, double check
- * the console output to be sure.
- * 4. Set your breakpoints in your CorDapp code.
- * 5. Run the "Debug CorDapp" remote debug run configuration.
  */
 public class NodeDriver {
     public static void main(String[] args) {
         final User user = new User("user1", "test", ImmutableSet.of("ALL"));
-        driver(new DriverParameters().withIsDebug(true).withWaitForAllNodesToFinish(true), dsl -> {
-                    CordaFuture<NodeHandle> partyAFuture = dsl.startNode(new NodeParameters()
-                            .withProvidedName(new CordaX500Name("PartyA", "London", "GB"))
-                            .withRpcUsers(ImmutableList.of(user)));
-                    CordaFuture<NodeHandle> partyBFuture = dsl.startNode(new NodeParameters()
-                            .withProvidedName(new CordaX500Name("PartyB", "New York", "US"))
-                            .withRpcUsers(ImmutableList.of(user)));
-                    CordaFuture<NodeHandle> partyCFuture = dsl.startNode(new NodeParameters()
-                            .withProvidedName(new CordaX500Name("PartyC", "Paris", "FR"))
-                            .withRpcUsers(ImmutableList.of(user)));
+        driver(new DriverParameters().withWaitForAllNodesToFinish(true), dsl -> {
+                    List<CordaFuture<NodeHandle>> nodeFutures = ImmutableList.of(
+                            dsl.startNode(new NodeParameters()
+                                    .withProvidedName(new CordaX500Name("PartyA", "London", "GB"))
+                                    .withCustomOverrides(ImmutableMap.of("rpcSettings.address", "localhost:10008", "rpcSettings.adminAddress", "localhost:10048", "webAddress", "localhost:10009"))
+                                    .withRpcUsers(ImmutableList.of(user))),
+                            dsl.startNode(new NodeParameters()
+                                    .withProvidedName(new CordaX500Name("PartyB", "New York", "US"))
+                                    .withCustomOverrides(ImmutableMap.of("rpcSettings.address", "localhost:10011", "rpcSettings.adminAddress", "localhost:10051", "webAddress", "localhost:10012"))
+                                    .withRpcUsers(ImmutableList.of(user))),
+                            dsl.startNode(new NodeParameters()
+                                    .withProvidedName(new CordaX500Name("PartyC", "Paris", "FR"))
+                                    .withCustomOverrides(ImmutableMap.of("rpcSettings.address", "localhost:10014", "rpcSettings.adminAddress", "localhost:10054", "webAddress", "localhost:10015"))
+                                    .withRpcUsers(ImmutableList.of(user))));
 
                     try {
-                        dsl.startWebserver(partyAFuture.get());
-                        dsl.startWebserver(partyBFuture.get());
-                        dsl.startWebserver(partyCFuture.get());
+                        dsl.startWebserver(nodeFutures.get(0).get());
+                        dsl.startWebserver(nodeFutures.get(1).get());
+                        dsl.startWebserver(nodeFutures.get(2).get());
+
                     } catch (Throwable e) {
                         System.err.println("Encountered exception in node startup: " + e.getMessage());
                         e.printStackTrace();
--- kotlin-source/src/test/kotlin/com/example/NodeDriver.kt
+++ kotlin-source/src/test/kotlin/com/example/NodeDriver.kt
@@ -7,28 +7,30 @@ import net.corda.testing.driver.driver
 import net.corda.testing.node.User
 
 /**
- * This file is exclusively for being able to run your nodes through an IDE (as opposed to using deployNodes)
+ * This file is exclusively for being able to run your nodes through an IDE.
  * Do not use in a production environment.
- *
- * To debug your CorDapp:
- *
- * 1. Run the "Run Example CorDapp - Kotlin" run configuration.
- * 2. Wait for all the nodes to start.
- * 3. Note the debug ports for each node, which should be output to the console. The "Debug CorDapp" configuration runs
- *    with port 5007, which should be "PartyA". In any case, double-check the console output to be sure.
- * 4. Set your breakpoints in your CorDapp code.
- * 5. Run the "Debug CorDapp" remote debug run configuration.
  */
 fun main(args: Array<String>) {
     val user = User("user1", "test", permissions = setOf("ALL"))
-    driver(DriverParameters(isDebug = true, waitForAllNodesToFinish = true)) {
-        val (partyA, partyB, partyC) = listOf(
-                startNode(providedName = CordaX500Name("PartyA", "London", "GB"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("PartyB", "New York", "US"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("PartyC", "Paris", "FR"), rpcUsers = listOf(user))).map { it.getOrThrow() }
+    driver(DriverParameters(waitForAllNodesToFinish = true)) {
+        val nodeFutures = listOf(
+                startNode(
+                        providedName = CordaX500Name("PartyA", "London", "GB"),
+                        customOverrides = mapOf("rpcSettings.address" to "localhost:10008", "rpcSettings.adminAddress" to "localhost:10048", "webAddress" to "localhost:10009"),
+                        rpcUsers = listOf(user)),
+                startNode(
+                        providedName = CordaX500Name("PartyB", "New York", "US"),
+                        customOverrides = mapOf("rpcSettings.address" to "localhost:10011", "rpcSettings.adminAddress" to "localhost:10051", "webAddress" to "localhost:10012"),
+                        rpcUsers = listOf(user)),
+                startNode(
+                        providedName = CordaX500Name("PartyC", "Paris", "FR"),
+                        customOverrides = mapOf("rpcSettings.address" to "localhost:10014", "rpcSettings.adminAddress" to "localhost:10054", "webAddress" to "localhost:10015"),
+                        rpcUsers = listOf(user)))
 
-        startWebserver(partyA)
-        startWebserver(partyB)
-        startWebserver(partyC)
+        val (nodeA, nodeB, nodeC) = nodeFutures.map { it.getOrThrow() }
+
+        startWebserver(nodeA)
+        startWebserver(nodeB)
+        startWebserver(nodeC)
     }
 }
